9f57a9af420ac606fa034e968eec03996a3cf2f8
CBB1017 <gtu1017@gmail.com>
2022-12-29 13:09:25 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+    private final TodoRepository repository;
+
+    public String testService() {
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        repository.save(entity);
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        return savedEntity.getTitle();
+    }
+
+}
